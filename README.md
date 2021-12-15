57c97a722ea788aba8fa0f3985a01cdb46663dba
Marcell Purham <mpurham@cybercell.local>
2021-12-15 13:38:55 -0600

    Added html formatter to vc

---
+++ README.md
@@ -0,0 +1,4 @@
+# HTML Formatter
+
+A simple html formatter for the Polypad Textpad.
+
---
+++ html-formatter.js
@@ -0,0 +1,13 @@
+/*
+    name: Format HTML
+    description: Cleans and format HTML documents.
+    author: Marcell
+    tags: html,prettify,clean,indent
+*/
+
+const { xml } = require('@polypad/vkBeautify')
+
+
+function main(state) {
+	state.text = xml(state.text)	
+}
